52a224a56bc6d8ddf665a5995a21638484eeb821
lgz940314 <529624928@qq.com>
2019-08-15 14:52:53 +0800

    使用非Eureka注册中心时使用@EnableDiscoveryClient 注解

--- krupp-gatewag/src/main/java/com/krupp/kruppgatewag/KruppGatewagApplication.java
+++ krupp-gatewag/src/main/java/com/krupp/kruppgatewag/KruppGatewagApplication.java
@@ -2,10 +2,12 @@ package com.krupp.kruppgatewag;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
+//@EnableDiscoveryClient //使用非Eureka注册中心时使用
 public class KruppGatewagApplication {
 
     public static void main(String[] args) {
